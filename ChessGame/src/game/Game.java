b7eed3ff4cf34c4bb6951e298ffe9437dbf489d1
benisagoldengod <cs1benito@gmail.com>
2015-05-27 10:28:04 -0700

    Worked on game piece jazz

--- ChessGame/src/game/Game.java
+++ ChessGame/src/game/Game.java
@@ -3,7 +3,16 @@ package game;
 import java.awt.Point;
 
 public class Game {
+	private GamePiece[][] board = new GamePiece[8][8];
+	private int Scorep1 = 0; // player 1 score
+	private int Scorep2 = 0; // player 2 score
+
 	public Game() {
+		// initialize pieces on board
+		board[0][0] = new GamePiece(3, false); // white rook
+		board[0][1] = new GamePiece(2, false); // white knight
+		board[0][2] = new GamePiece(1, false); // white bishop
+		board[0][3] = new GamePiece(4, false); // white queen
 
 	}
 
